26a3e1a289642cf04dc044510bae283d52b9051b
PrefixTim <developer.of.gift@gmail.com>
2020-03-11 18:00:48 -0700

    package controller

---
+++ wss-web/src/main/java/rclub/wss/web/PackageController.java
@@ -0,0 +1,23 @@
+package rclub.wss.web;
+
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import rclub.wss.domain.data.Package;
+import rclub.wss.service.PackageService;
+
+@RestController
+@RequestMapping("/api")
+@AllArgsConstructor
+public class PackageController {
+    @Autowired
+    private PackageService service;
+
+    @PostMapping("/add")
+    public Package addPackage(@RequestBody Package p) {
+        return service.addPackage(p);
+    }
+}

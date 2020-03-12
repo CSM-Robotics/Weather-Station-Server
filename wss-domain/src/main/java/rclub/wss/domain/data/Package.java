8ceed304f7382b937792fd9a4dafbd6c3e0483eb
PrefixTim <developer.of.gift@gmail.com>
2020-03-11 17:47:10 -0700

    packages dependensy changed

--- wss-domain/src/main/java/rclub/wss/domain/data/Package.java
+++ wss-domain/src/main/java/rclub/wss/domain/data/Package.java
@@ -1,20 +1,40 @@
 package rclub.wss.domain.data;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
 /**
  * simple class representing data from the ras pi
- *
+ * <p>
  * must be separated to different files in later versions
- *
+ * <p>
  * variables names are unpleasant
  */
+@Entity
+@Data
+@Table(name = "packages")
 public class Package {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column
     private float temp;
+    @Column
     private float pressPa;
+    @Column
     private float hum;
+    @Column
     private float CO2ppm;
+    @Column
     private float tVOCpp;
+    @Column
     private long count;
+    @Column
     private long packetNum;
+    @Column
     private int deviceInfo;
-
+    @Version
+    private Long version;
 }
--- wss-persistance/src/main/java/rclub/wss/persistance/rep/PackageRep.java
+++ wss-persistance/src/main/java/rclub/wss/persistance/rep/PackageRep.java
@@ -2,8 +2,8 @@ package rclub.wss.persistance.rep;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import rclub.wss.persistance.dto.PackageDTO;
+import rclub.wss.domain.data.Package;
 
 @Repository
-public interface PackageRep extends CrudRepository<PackageDTO, Integer> {
+public interface PackageRep extends CrudRepository<Package, Long> {
 }
